4aff4395cb02e6299ffec7db3b8005b274c18a03
Niikcety <nikolaineshev1999@gmail.com>
2020-05-18 19:41:41 +0300

    added traversal function

--- main.py
+++ main.py
@@ -2,32 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
-raw_links = []
-valid_links = []
+all_links = []
 
 
-def crawl(link="https://register.start.bg/"):
+def crawl(link="https://hackbulgaria.com/"):
+    raw_links = []
     try:
         r = requests.get(link, timeout=5)
     except requests.exceptions.ReadTimeout:
         return []
     soup = BeautifulSoup(r.text, features="html.parser")
     for row in soup.find_all('a'):
-        raw_links.append(row.get('href'))
+        link = row.get('href')
+        if link is not None and link[0:4] == 'http':
+            print(link)
+            raw_links.append(link)
     return raw_links
 
 
-def parse_crawled(links):
+def traverse_through_links(links):
     for link in links:
-        if link is not None and link[0:4] == 'http':
-            print(link)
-            valid_links.append(link)
-    return valid_links
+        links += crawl(link)
+    return links
 
 
 if __name__ == '__main__':
-    crawled = crawl()
-    valid = parse_crawled(crawled)
-    for link in valid:
-        valid_links += parse_crawled(crawl(link))
-
+    # for link in valid:
+    #     valid_links += parse_crawled(crawl(link))
+    all_links = traverse_through_links(crawl('https://register.start.bg/'))
