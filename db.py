ae6308b17d09fce63157e836c7e4ab4a215364cb
Niikcety <nikolaineshev1999@gmail.com>
2020-05-20 14:14:22 +0300

    Added db functions

--- db.py
+++ db.py
@@ -3,14 +3,21 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 Base = declarative_base()
 
 
-class Links(Base):
+class VisitedLinks(Base):
     __tablename__ = "visited"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     server_type = Column(String)
+
+
+class NotVisitedLinks(Base):
+    __tablename__ = "not_visited"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
 
 engine = create_engine("sqlite:///urls.db")
--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
-from db import Base, session, Links
+from db import Base, session, VisitedLinks, NotVisitedLinks
 
 
-all_links = []
+
 
 
 def crawl(link="https://register.start.bg/"):
-    raw_links = []
+    queue_links = []
     try:
         r = requests.get(link, timeout=5)
     except Exception:
-        return []
+        return
     soup = BeautifulSoup(r.text, features="html.parser")
     for row in soup.find_all('a'):
         link_row = row.get('href')
         if link_row is not None:
             if link_row[0:4] == 'http':
-                print(link_row)
+                queue_links.append(NotVisitedLinks(name=link_row))
             elif link_row[0:4] == 'link':
-                raw_links.append(link + link_row)
-    return raw_links
+                queue_links.append(NotVisitedLinks(name=link + link_row))
+    session.add_all(queue_links)
+    session.commit()
 
 
 def get_server_type(link):
@@ -31,17 +32,44 @@ def get_server_type(link):
         return 'Unknown'
 
 
-def traverse_through_links(links):
-    visited = []
-    for link in links:
-        if link not in visited:
-            session.add(Links(name=link, server_type=get_server_type(link)))
+def check_if_not_visited(link):
+    visited = session.query(VisitedLinks).filter(VisitedLinks.name == link)
+    try:
+        visited.name
+    except AttributeError:
+        return True
+    return False
+
+
+def traverse_through_links():
+    while True:
+        first_in_queue = session.query(NotVisitedLinks).first()
+        if check_if_not_visited(first_in_queue.name):
+            visited = VisitedLinks(name=first_in_queue.name, server_type=get_server_type(first_in_queue.name))
+            session.add(visited)
             session.commit()
-            visited.append(link)
-            links += crawl(link)
-    return links
+            crawl(first_in_queue.name)
+        session.delete(first_in_queue)
+        session.commit()
+
+
+def check_if_db_is_empty():
+    visited_count = session.query(VisitedLinks).count()
+    not_visited_count = session.query(NotVisitedLinks).count()
+
+    return (visited_count + not_visited_count == 0, visited_count, not_visited_count)
+
+
+def start():
+    check_if_empty = check_if_db_is_empty()
+    if check_if_empty[0]:
+        name_of_crawl_start = input('Please enter the name of the site you want to crawl: ')
+        crawl(name_of_crawl_start)
+    else:
+        print('They are {} visited sites and {} in the queue'.format(check_if_empty[1], check_if_empty[2]))
+    traverse_through_links()
 
 
 if __name__ == '__main__':
-    start_links = crawl()
-    traverse_through_links(start_links)
+    start()
+
